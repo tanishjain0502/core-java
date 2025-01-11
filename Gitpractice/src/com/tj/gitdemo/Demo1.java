fa0e3bcc0dc4dc7ec1db225ea30cdb10893b03fd
Tanish Jain <tanishjain0502@gmail.com>
2025-01-12 03:25:24 +0530

    added print msg

--- Gitpractice/src/com/tj/gitdemo/Demo1.java
+++ Gitpractice/src/com/tj/gitdemo/Demo1.java
@@ -3,7 +3,7 @@ package com.tj.gitdemo;
 public class Demo1 {
 
 	public static void main(String[] args) {
-     		System.out.println("hello world");
+     		System.out.println("hello world123");
 		
 	}
 
